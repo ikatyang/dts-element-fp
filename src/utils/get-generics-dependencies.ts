cf91631067b618e6b9bcab8189e74693b0b5c266
ikatyang <ikatyang@gmail.com>
2017-08-08 23:02:58 +0800

    fix: deep generics dependencies

--- src/utils/get-generics-dependencies.ts
+++ src/utils/get-generics-dependencies.ts
@@ -1,17 +1,35 @@
 import * as dts from 'dts-element';
+import * as R from 'ramda';
 import { has } from './has';
 
+// tslint:disable-next-line:no-unused-variable
 import { IGenericDeclaration } from 'dts-element';
 
 export const get_generics_dependencies = (
   generics: dts.IGenericDeclaration[],
-) =>
-  generics.map(generic => [
-    generic,
-    ...generics.filter(current_generic =>
-      has(generic, {
-        kind: dts.ElementKind.GeneralType,
-        name: current_generic.name,
-      }),
-    ),
-  ]);
+) => {
+  const generics_dependencies: any[] = generics.map(x => [x]);
+
+  generics.forEach((generic, index) => {
+    generics.forEach((current_generic, current_index) => {
+      if (
+        generic === current_generic ||
+        !has(generic, {
+          kind: dts.ElementKind.GeneralType,
+          name: current_generic.name,
+        })
+      ) {
+        return;
+      }
+
+      generics_dependencies[index].push(
+        current_generic,
+        generics_dependencies[current_index],
+      );
+    });
+  });
+
+  return generics_dependencies.map(
+    R.pipe(R.flatten, R.uniq),
+  ) as dts.IGenericDeclaration[][];
+};
