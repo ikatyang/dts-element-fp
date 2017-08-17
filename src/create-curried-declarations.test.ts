4b5034d23f43d7a303c0e6a6359978ff8ce77fde
Ika <ikatyang@gmail.com>
2017-08-17 23:51:03 +0800

    fix: remove unnecessary types for `placeholder: false` (#18)

--- src/create-curried-declarations.test.ts
+++ src/create-curried-declarations.test.ts
@@ -1,5 +1,6 @@
 import * as dts from 'dts-element';
 import { create_curried_declarations } from './create-curried-declarations';
+import { CreateCurriedDeclarationsOptions } from './create-curried-types';
 import { parse_types } from './utils/parse-types';
 
 const test_cases = {
@@ -13,7 +14,9 @@ const test_cases = {
 };
 
 it('should transform correctly with type = FunctionType', () => {
-  expect(emit_curried_declarations(test_cases.normal, true)).toMatchSnapshot();
+  expect(
+    emit_curried_declarations(test_cases.normal, { use_first_type: true }),
+  ).toMatchSnapshot();
 });
 
 it('should transform correctly with type = Record<string, FunctionType> (= 1 key)', () => {
@@ -24,12 +27,32 @@ it('should transform correctly with type = Record<string, FunctionType> (> 1 key
   expect(emit_curried_declarations(test_cases.various)).toMatchSnapshot();
 });
 
-function emit_curried_declarations(test_case: string, use_first_type = false) {
+it('should omit unnecessary types with placeholder = false and type = FunctionType', () => {
+  expect(
+    emit_curried_declarations(test_cases.normal, { placeholder: false }),
+  ).toMatchSnapshot();
+});
+
+it('should omit unnecessary types with placeholder = false and type = Record<string, FunctionType>', () => {
+  expect(
+    emit_curried_declarations(test_cases.various, { placeholder: false }),
+  ).toMatchSnapshot();
+});
+
+function emit_curried_declarations(
+  test_case: string,
+  raw_options: CreateCurriedDeclarationsOptions & {
+    use_first_type?: boolean;
+  } = {},
+) {
   const types = parse_types(test_case);
+
+  const { use_first_type = false } = raw_options;
 
   const options = {
     selectable: true,
     placeholder: true,
+    ...raw_options,
   };
 
   const curried_types = use_first_type
